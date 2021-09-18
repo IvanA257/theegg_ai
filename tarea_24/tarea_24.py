42876c1a4607e4438df2d842592121a2c5fe35d0
ivanA257 <ivanalbarez@gmail.com>
2021-09-18 11:11:51 +0200

    Minor changes to the code + Wrote more docs.

--- tarea_24/tarea_24.py
+++ tarea_24/tarea_24.py
@@ -2,19 +2,19 @@ from math import sin
 import matplotlib.pyplot as plt
 
 
-def adc(value: float, bits: int, __range: tuple, __returned):
+def adc(value: float, bits: int, input_range: tuple, returned: list):
     """
     This function takes an input value and a value range and converts it to a position represented by n bits.
     :param value: The analog value we want to convert to digital.
     :param bits: The amount of bits we'll use to represent the digital output.
-    :param __range: Tuple with the minimum and maximum values analog values we are to receive.
-    :param __returned: Digital output.
+    :param input_range: Tuple with the minimum and maximum analog values we are to receive.
+    :param returned: Digital output.
     :return:
     """
 
     # Calculate the maximum weight we'll have to deal with. This is the distance between the minimum possible input and
     # the maximum one.
-    width = __range[1] - __range[0]
+    width = input_range[1] - input_range[0]
 
     # Now, we divide that total width between the total amount of states we can make of the bits we have.
     # The bit height is the distance between two bit positions.
@@ -24,13 +24,13 @@ def adc(value: float, bits: int, __range: tuple, __returned):
     # Imagine we want to reach a certain height stacking up Lego pieces, without exceeding that height.
     # The amount of pieces we'd need would be the integer division between the given height (measured from the floor)
     # and the height of each Lego piece.
-    position = int(((value - __range[0]) // bit_height))
+    position = int(((value - input_range[0]) // bit_height))
 
     # Put it in the proper binary format.
     print("{0:0{width}b}".format(position, width=bits))
 
     # I think this is what should be returned,  a n bit binary value.
-    __returned.append(position)
+    returned.append(position)
 
     # This, on the other hand, is a more visually representative one.
     return (value // bit_height)*bit_height
